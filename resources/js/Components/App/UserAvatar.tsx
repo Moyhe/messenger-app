ff81b295f80b1ddb54aceafee0b382f28f0e72d1
Mohye <dragonsinmeliodas777@gmail.com>
2024-06-29 19:52:05 +0300

    fix avatar width

--- resources/js/Components/App/UserAvatar.tsx
+++ resources/js/Components/App/UserAvatar.tsx
@@ -10,7 +10,7 @@ const UserAvatar = ({ user, online, profile }: Props) => {
     const onlineClass =
         online() == true ? "online" : online() == false ? "offline" : "";
 
-    const sizeClass = profile ? "w-40" : "w-8";
+    const sizeClass = profile ? "w-40" : "w-10";
 
     return (
         <>
